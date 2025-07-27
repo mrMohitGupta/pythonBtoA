c08943cb83871628ad44ab091fd0e6bebeaa3729
Mohit Gupta <mohit16012004@gmail.com>
2025-07-28 01:44:55 +0530

    Add files via upload

---
+++ 01_input.py
@@ -0,0 +1,8 @@
+a = input("Enter a number")
+print(a)
+
+b = input("Enter your name: ")
+print(b)
+
+c = input("enter a number")
+print(c)
---
+++ 02_add.py
@@ -0,0 +1,13 @@
+# a = input("Enter first number") 
+# a = int(a) # convert a to int version of a
+# print(a + 3)
+
+a = int(input("Enter first number"))
+# a = int(a)
+b = int(input("Enter second number"))
+# b = int(b)
+
+print(a + b)
+
+c =input("enter a number")
+print(c)
